4a3e28f55b5efd7f77a6f6f81dc83c72b6005480
Padmapiyush <padmpiyush2801@gmail.com>
2023-02-11 09:58:43 +0530

    only scripts

--- script.py
+++ script.py
@@ -1,27 +1,6 @@
-import win32api
 import tabula
-from flask import Flask, render_template
 
-app = Flask(__name__)
+filename = input("Enter File Path: ")
+df = tabula.read_pdf(filename, encoding='utf-8', spreadsheet=True, pages='1')
 
-
-#Using the below, the popup message appears on the page load of index.html
-#0x00001000 - This makes the popup appear over the browser window
-@app.route('/')
-def index():
-    win32api.MessageBox(0, 'You have just run a python script on the page load!', 'Running a Python Script via Javascript', 0x00001000)
-    return render_template('index.html')
-
-#Using the below, the popup message appears when the button is clicked on the webpage.
-#0x00001000 - This makes the popup appear over the browser window
-@app.route('/test')
-def test():
-    win32api.MessageBox(0, 'You have just run a python script on the button press!', 'Running a Python Script via Javascript', 0x00001000)
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
-#below is script
-filename = getvalue('file')
-df = tabula.read_pdf(filename, encoding='utf-8', spreadsheet=True, pages='all')
-df.to_csv('file.csv')
+df.to_csv('output.csv')
